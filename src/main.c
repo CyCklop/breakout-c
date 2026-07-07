659c5132c1a60aa0bb22bcb9a4dacd184d832c2f
CyCklop <ignacioandresflores06@gmail.com>
2026-07-07 18:59:37 -0400

    se agrego el bucle de la ventana en el main.c

--- src/main.c
+++ src/main.c
@@ -2,5 +2,20 @@
 
 int main(void)
 {
+    const int anchoVentana = 800;
+    const int altoVentana = 600;
+
+    InitWindow(anchoVentana, altoVentana, "breakout");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
