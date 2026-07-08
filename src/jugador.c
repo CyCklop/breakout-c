598f5f7c338b50c4799a2ba92b85338ec502b7d4
CyCklop <ignacioandresflores06@gmail.com>
2026-07-08 01:16:28 -0400

    a0.0.5/ se agrego la logica a la funcion de mover_jugador

--- src/jugador.c
+++ src/jugador.c
@@ -1,4 +1,15 @@
 #include <raylib.h>
 #include "../includes/jugador.h"
 
-//agregar logica de metodos
+void mover_jugador(Jugador *jugador)
+{
+    if (IsKeyDown(KEY_A))
+    {
+        jugador->posicion.x -= jugador->velocidad;
+    }
+
+    if (IsKeyDown(KEY_D))
+    {
+        jugador->posicion.x += jugador->velocidad;
+    }
+}
